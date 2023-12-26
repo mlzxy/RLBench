dc371c5f5649f05ccd1ce83e84bb0334b3fceb28
Xinyu Zhang <xz653@rutgers.edu>
2023-12-26 01:26:26 -0500

    add gen vis

---
+++ gen.vis.sh
@@ -0,0 +1,6 @@
+python3  tools/dataset_generator.py  --tasks=block_pyramid \
+        --save_path=/media/xinyu/UDriveLinux/tmp/rlbench_novel/vis/ \
+        --image_size=840,840 \
+        --renderer=opengl \
+        --episodes_per_task=5 \
+        --processes=1
